0f4560bf9f8628a259ba1de4fd467dbbca6e9701
Jeff Harris <jeff@jeffreysharris.com>
2014-11-20 15:42:42 -0500

    Merged common.js, rebuilt app

---
+++ .meteor/local/build/programs/server/app/ProfessorPeanut.js
@@ -0,0 +1,15 @@
+(function(){if (Meteor.isClient) {
+	/*
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_ONLY"
+	});
+	*/
+};
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+};
+
+})();
---
+++ .meteor/local/build/programs/server/packages/bootstrap-3.js
@@ -0,0 +1,14 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['bootstrap-3'] = {};
+
+})();
+
+//# sourceMappingURL=bootstrap-3.js.map
