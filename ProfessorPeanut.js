0195abd3e39edc2638df7e58157fa4858bcd99a8
Jeff Harris <jeff@jeffreysharris.com>
2014-11-21 00:56:41 -0500

    Routing enabled with iron:router package
    Broke up html and js concerns. Templates.html are dynamically routed in
    via routes.js to ProfessorPeanut.html, which is now just a shell
    layout. Pro…Peanut.js contains the client/server Meteor functions, like
    creating users and logging in.

--- ProfessorPeanut.js
+++ ProfessorPeanut.js
@@ -1,9 +1,43 @@
 if (Meteor.isClient) {
-	/*
-	Accounts.ui.config({
-		passwordSignupFields: "USERNAME_ONLY"
+	/*TEMPLATE HELPERS*/
+	Template.navItems.helpers({
+	    activeIfTemplateIs: function (template) {
+	      var currentRoute = Router.current();
+	      return currentRoute &&
+	      template === currentRoute.lookupTemplate() ? 'active' : '';
+	    }
+	  });
+
+	/*USER ACCOUNT CREATION, LOGIN, LOGOUT*/
+	Template.register.events({
+		'submit form': function(e, template){
+			e.preventDefault();
+			var username_var = template.find("#register-username").value;
+			var email_var = template.find("#register-email").value;
+			var password_var = template.find("#register-password").value;
+			Accounts.createUser({
+				username: username_var,
+				email: email_var,
+				password: password_var
+			});
+			console.log("Submitted")
+		}
 	});
-	*/
+	Template.login.events({
+		'submit form': function(e, template){
+			e.preventDefault();
+			var email_var = template.find("#login-email").value;
+			var password_var = template.find("#login-password").value;
+			Meteor.loginWithPassword(email_var, password_var);
+			console.log("Submitted")
+		}
+	});
+	Template.dashboard.events({
+		'click .logout' : function(e){
+			e.preventDefault();
+			Meteor.logout();
+		}
+	});
 };
 
 if (Meteor.isServer) {
