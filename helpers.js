75f84c6009a89a7e030c906311468a6aefd70388
Jeff Harris <jeff@jeffreysharris.com>
2014-11-24 00:41:37 -0500

    Requests now correctly populate
    Prettied up the dates with moment.js
    Look up the requestor name by cross referencing requestorID against
    Meteor.users

--- helpers.js
+++ helpers.js
@@ -1,8 +1,9 @@
 if (Meteor.isClient) {
+
 	Template.dashboard.helpers({
 		fullName: function(){
 			return Meteor.user().profile.name.first + " " + Meteor.user().profile.name.last;
-		},
+		}
 	});
 	Template.myProfile.helpers({
 		firstName: function(){
@@ -10,8 +11,24 @@ if (Meteor.isClient) {
 		}
 	});
 	Template.requests.helpers({
-		requests: function(){
-			return Requests.find();
+		request: function(){
+			return Requests.find({});
+		},
+		requestor: function(requestorID){
+			if(typeof requestorID !== null){
+				var user = Meteor.users.findOne({"_id": requestorID});
+				if(typeof user !== null){
+					var name = user.profile.name.first + " " +user.profile.name.last;
+					return name;
+				} else return;
+			} else return;
+		},
+		dateConvert: function(date){
+			if(typeof date !== null) {
+				var humaneDate = moment(date).format("dddd, MMMM Do YYYY, h:mm a");
+				//console.log(humaneDate);
+				return humaneDate;
+			} else return;
 		}
 	});
 };
