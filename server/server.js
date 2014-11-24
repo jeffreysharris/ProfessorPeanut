a855e78f656941210b1eace64bfa1b980c351b17
jmmarti <jmmartin612@gmail.com>
2014-11-24 15:26:09 -0500

    Added 'createRequest' method
    Figured out where to put meteor methods - built in callbacks too!
    
    call the function with:
    Meteor.call( 'createRequest' , args, function(error, result){});
    
    check server.js to see what args should look like. I thought it would be
    better as an object than one long ass set of params. If there's a more
    convenient way to pass the parameters from your perspective let me know!
    
    Next up:
    -add login check to createRequest - it currently breaks without an
    userId
    -move other global functions to Meteor.methods
    -migrate other global functions to

--- server/server.js
+++ server/server.js
@@ -1,11 +1,67 @@
+Meteor.methods({
+	createRequest : function ( args ){
+
+		//ensure the args are formatted correctly 
+		//(not sure if order matters)
+		check(args, {
+			title: String,
+			description: String,
+			targetFunding: Number
+		});
+
+		//if the method is called from a client, use their userId to create
+		//the reqeust. If the method is called from the server (this.connection = null)
+		//then get a random user to pair. 
+		//TODO: add a login check
+		var requestor = {};
+		if( this.connection ){
+			requestor = Meteor.users.findOne({ _id : this.userId });
+		}else{
+			requestor = getRandomDoc( Meteor.users );
+		}
+
+		var request = {
+			title : args.title,
+			description : args.description,
+			startDate : moment().format(),
+			endDate : moment().add( 'months', 1 ),
+			targetFunding : args.targetFunding,
+			requestorID : requestor._id,
+		};
+
+		var requestID = Requests.insert(	
+							request,
+							{validationContext : "insertForm"},
+							function( error, result ){
+								if( error ){
+									console.log( error );
+								}else{
+									//console.log( result );
+								}
+							});
+
+		requestor.profile.postedRequests.push( requestID );
+		Meteor.users.update(
+			{ _id : requestor._id },
+			{ $set : { "profile.postedRequests" : requestor.profile.postedRequests }},
+			{ validationContext: "updateForm" },
+			function( error, result ){
+				if( error ){
+					console.log( error );
+				}else{
+					//console.log( result );
+				}
+			});
+	}
+});
+
 Meteor.startup(function (){
 
-
-	//REMOVE THESE TO RETAIN NEW DB ENTRIES
-	console.log('Clearing database... check server/server.js[ln:5] to stop this from happening.')
-	Meteor.users.remove({});
-	Requests.remove({});
-	Transactions.remove({});
+	// REMOVE THESE TO RETAIN NEW DB ENTRIES
+	// console.log('Clearing database... check server/server.js[ln:5] to stop this from happening.')
+	// Meteor.users.remove({});
+	// Requests.remove({});
+	// Transactions.remove({});
 
 
 	//If Users collection is empty, fill it with seed!
@@ -83,8 +139,8 @@ Meteor.startup(function (){
 	 						//console.log( result );
 	 					}
 	 				}
-	 			)
-	 	};
+	 			);
+	 	}
 	}
 
 	if( Transactions.find().count() === 0 ){
@@ -100,6 +156,6 @@ Meteor.startup(function (){
 			doTransaction( request._id, donor._id, (Math.round((Math.random()*maxAmount)*2)/2) );
 
 			}		
-		};		
+		}
 	}
 });
