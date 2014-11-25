b8ecc1c910d3e670db6f127920ca01ecace9abc0
Jeff Harris <jeff@jeffreysharris.com>
2014-11-25 01:25:19 -0500

    client.js move

--- client/client.js
+++ client/client.js
@@ -1,7 +1,11 @@
 Session.setDefault('currentPage', 0);
 Meteor.autorun(function(){
 	Meteor.subscribe('page', Session.get('currentPage'));
+	Meteor.subscribe('myRequests', Meteor.user()._id);
 });
 Template.requests.request = function(){
+	return Requests.find({});
+};
+Template.myProfile.findRequests = function(){
 	return Requests.find({});
 };
